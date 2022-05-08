e209c1bb28d119bff54a10d9ca34b11845806e41
moppurin <98684224+moppurin@users.noreply.github.com>
2022-05-08 16:14:58 +0900

    [add]sprite,particle

--- template/template.js
+++ template/template.js
@@ -1,11 +1,13 @@
 var canvas, g;
-var characterPosX, characterPosY, characterImage, characterR;
-var speed, acceleration;
-var enemyPosX, enemyPosY, enemyImage, enemySpeed, enemyR;
+// var characterPosX, characterPosY, characterImage, characterR;
+// var speed, acceleration;
+// var enemyPosX, enemyPosY, enemyImage, enemySpeed, enemyR;
+var player, enemy;
 var score;
 var scene;
 var frameCount;
 var bound;
+var particles;
 
 // シーンの定義
 const Scenes = {
@@ -26,34 +28,49 @@ onload = function () {
 };
 
 function init() {
-  characterPosX = 100;
-  characterPosY = 400;
-  characterR = 16;
-  characterImage = new Image();
-  characterImage.src = "./reimu.png";
-  speed = 0;
-  acceleration = 0;
+  // 自キャラ初期化
+  player = new Sprite();
+  player.posx = 100;
+  player.posy = 400;
+  player.r = 16;
+  player.image = new Image();
+  player.image.src = "./reimu.png";
+  player.speed = 0;
+  player.acceleration = 0;
 
-  enemyPosX = 600;
-  enemyPosY = 400;
-  enemyR = 16;
-  enemyImage = new Image();
-  enemyImage.src = "./marisa.png";
-  enemySpeed = 5;
-
-  // スコア初期化
-  score = 0;
+  // 敵キャラ初期化
+  enemy = new Sprite();
+  enemy.posx = 600;
+  enemy.posy = 400;
+  enemy.r = 16;
+  enemy.image = new Image();
+  enemy.image.src = "./marisa.png";
+  enemy.speed = 5;
+  enemy.acceleration = 0;
 
   // ゲーム管理データの初期化
   score = 0;
   frameCount = 0;
   bound = false;
   scene = Scenes.GameMain;
+
+  // パーティクル初期化
+  particles = [];
 }
 
 function keydown(e) {
-  speed = -20;
-  acceleration = 1.5;
+  // ゲームプレイ中
+  if (scene == Scenes.GameMain) {
+    if (player.speed == 0) {
+      player.speed = -20;
+      player.acceleration = 1.5;
+    }
+    // ゲームオーバー中
+  } else if (scene == Scenes.GameOver) {
+    if (frameCount > 60) {
+      init();
+    }
+  }
 }
 
 function gameloop() {
@@ -65,49 +82,60 @@ function update() {
   // ゲームプレイ中
   if (scene == Scenes.GameMain) {
     // 自キャラの状態更新
-    speed = speed + acceleration;
-    characterPosY = characterPosY + speed;
-    if (characterPosY > 400) {
-      characterPosY = 400;
-      speed = 0;
-      acceleration = 0;
+    player.speed = player.speed + player.acceleration;
+    player.posy = player.posy + player.speed;
+    if (player.posy > 400) {
+      player.posy = 400;
+      player.speed = 0;
+      player.acceleration = 0;
     }
 
     // 敵キャラの状態更新
-    enemyPosX -= enemySpeed;
-    if (enemyPosX < -100) {
-      enemyPosX = 600;
+    enemy.posx -= enemy.speed;
+    if (enemy.posx < -100) {
+      enemy.posx = 600;
       score += 100;
     }
 
     // 当たり判定
-    var diffX = characterPosX - enemyPosX;
-    var diffY = characterPosY - enemyPosY;
+    var diffX = player.posx - enemy.posx;
+    var diffY = player.posy - enemy.posy;
     var distance = Math.sqrt(diffX * diffX + diffY * diffY);
-    if (distance < characterR + enemyR) {
+    if (distance < player.r + enemy.r) {
       // 当たった時の処理
       scene = Scenes.GameOver;
-      speed = -20;
-      acceleration = 0.5;
+      // player.speed = -20;
+      // player.acceleration = 0.5;
       frameCount = 0;
+
+      // パーティクル生成
+      for (var i = 0; i < 300; i ++) {
+        particles.push(new Particle(player.posx, player.posy))
+      }
     }
+
     // ゲームオーバー中
   } else if (scene == Scenes.GameOver) {  
     // 自キャラの状態更新
-    speed = speed + acceleration;
-    characterPosY = characterPosY + speed;
+    // player.speed = player.speed + player.acceleration;
+    // player.posy = player.posy + player.speed;
     
-    if (characterPosX < 20 || characterPosX > 460) {
-      bound = !bound;
-    }
-    if (bound) {
-      characterPosX = characterPosX + 30;
-    } else {
-      characterPosX = characterPosX - 30;
-    }
+    // if (player.posx < 20 || player.posx > 460) {
+    //   bound = !bound;
+    // }
+    // if (bound) {
+    //   player.posx = player.posx + 30;
+    // } else {
+    //   player.posx = player.posx - 30;
+    // }
+
+    // パーティクルの状態更新
+    particles.forEach((p) => {
+      p.update();
+    });
 
     // 敵キャラの状態更新
-    enemyPosX += enemySpeed;
+    enemy.posx -= enemy.speed;
   }
 
     // 現在何フレーム目かをカウント
@@ -125,18 +153,20 @@ function draw() {
     g.fillRect(0, 0, 480, 480);
 
     // キャラクター描画
-    g.drawImage(
-      characterImage,
-      characterPosX - characterImage.width / 2,
-      characterPosY - characterImage.height / 2
-    );
+    // g.drawImage(
+    //   player.image,
+    //   player.posx - player.image.width / 2,
+    //   player.posy - player.image.height / 2
+    // );
+    player.draw(g);
 
     // 敵キャラクター描画
-    g.drawImage(
-      enemyImage,
-      enemyPosX - enemyImage.width / 2,
-      enemyPosY - enemyImage.height / 2
-    );
+    // g.drawImage(
+    //   enemy.image,
+    //   enemy.posx - enemy.image.width / 2,
+    //   enemy.posy - enemy.image.height / 2
+    // );
+    enemy.draw(g);
 
     // スコア描画
     g.fillStyle = "rgb(255,255,255)";
@@ -144,25 +174,42 @@ function draw() {
     var scoreLabel = "SCORE : " + score;
     var scoreLabelWidth = g.measureText(scoreLabel).width;
     g.fillText(scoreLabel, 460 - scoreLabelWidth, 40);
+
+    // ゲームオーバー中
   } else if (scene == Scenes.GameOver) {
+
     // 背景描画
     g.fillStyle = "rgb(0,0,0)";
     g.fillRect(0, 0, 480, 480);
-
+    
     // キャラクター描画
-    if (frameCount < 120) {
-      g.save();
-      g.translate(characterPosX, characterPosY);
-      g.rotate(((frameCount % 30) * Math.PI * 2) / 30);
-      g.drawImage(
-        characterImage,
-        -characterImage.width / 2,
-        -characterImage.height / 2,
-        characterImage.width + frameCount,
-        characterImage.height + frameCount
-      );
-      g.restore();
-    }
+    // if (frameCount < 120) {
+    //   g.save();
+    //   g.translate(player.posx, player.posy);
+    //   g.rotate(((frameCount % 30) * Math.PI * 2) / 30);
+    //   g.drawImage(
+    //     player.image,
+    //     -player.image.width / 2,
+    //     -player.image.height / 2,
+    //     player.image.width + frameCount,
+    //     player.image.height + frameCount
+    //   );
+    //   g.restore();
+    // }
+
+    // パーティクル描画
+    particles.forEach((p) => {
+      p.draw(g);
+    });
+
+
+   // 敵キャラクター描画
+  //  g.drawImage(
+  //   enemy.image,
+  //   enemy.posx - enemy.image.width / 2,
+  //   enemy.posy - enemy.image.height / 2
+  // );
+  enemy.draw(g);
 
     // スコア描画
     g.fillStyle = "rgb(255,255,255)";
@@ -177,5 +224,60 @@ function draw() {
     var scoreLabel = "GAME OVER";
     var scoreLabelWidth = g.measureText(scoreLabel).width;
     g.fillText(scoreLabel, 240 - scoreLabelWidth / 2, 240);
+  }
+}
+
+// スプレイトクラス
+class Sprite {
+  image = null;
+  posx = 0;
+  posy = 0;
+  speed = 0;
+  acceleration = 0;
+  r = 0;
+
+  draw(g) {
+    g.drawImage(
+      this.image,
+      this.posx - this.image.width / 2,
+      this.posy - this.image.height / 2
+    );
+  }
+}
+
+// パーティクルクラス
+class Particle extends Sprite {
+  baseLine = 0;
+  acceleration = 0;
+  speedy = 0;
+  speedx = 0;
+
+  constructor(x, y) {
+    super();
+    this.posx = x;
+    this.posy = y;
+    this.baseLine = 420;
+    this.acceleration = 0.5;
+    var angle = (Math.PI * 5) / 4 + (Math.PI / 2) * Math.random();
+    this.speed = 5 + Math.random() * 20;
+    this.speedx = this.speed * Math.cos(angle);
+    this.speedy = this.speed * Math.sin(angle);
+    this.r = 2;
+  }
+
+  update() {
+    this.speedx *= 0.97;
+    this.speedy += this.acceleration;
+    this.posx += this.speedx;
+    this.posy += this.speedy;
+    if (this.posy > this.baseLine) {
+      this.posy = this.baseLine;
+      this.speedy = this.speedy * -1 * (Math.random() * 0.5 + 0.3);
+    }
+  }
+
+  draw(g) {
+    g.fillStyle = "rgb(255,50,50)";
+    g.fillRect(this.posx - this.r, this.posy - this.r, this.r * 2, this.r * 2);
   }
 }
